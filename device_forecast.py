60471db19fb890dea75430d2d6d0a11ddfae4b0f
Gabrie van Zanten <thegabeman@gmail.com>
2022-10-15 14:54:06 +0200

    Added an option to add multiple devices Added Error handling

--- device_forecast.py
+++ device_forecast.py
@@ -1,4 +1,5 @@
 import json
+import sys
 import requests
 from datetime import date, time, datetime
 
@@ -14,7 +15,13 @@ response = requests.get(
                     url= api_url
 )
 jsonresponse = response.json()
-
+if jsonresponse['result'] == None:
+    print( "No results")
+    print( "Respone Code : ",  jsonresponse['message']['code'] )
+    print( "Respone type : ",  jsonresponse['message']['type'] )
+    print( "Respone text : ",  jsonresponse['message']['text'] )
+    print( "Retry at : ",  jsonresponse['message']['ratelimit']['retry-at'] )
+    sys.exit()
 ## watts - Watts (power) average for the period
 ## watt_hours - Watt hours (energy) summarized over the day
 ## watt_hours_period - Watt hours (energy) for the period
@@ -22,7 +29,7 @@ jsonresponse = response.json()
 ## To see at what time you can start an heavy power user, we look at watt_hours_period
 
 ## What is the Watt Hour of your device. (Hourly)
-device_watt_hour = 830             ## Afwasmachine = 832 Wh (gemiddeld)
+devices_watt_hour = [ ["dish washer",832] , ["washing machine",1400], ["dryer",2000] ]
 
 ## What date is it?
 today = date.today()
@@ -33,9 +40,10 @@ print("*** Forecast in Watts per hour ***")
 watt_hours_period = day_estimates['watt_hours_period']
 for timestamp,value in watt_hours_period.items():
     if date.today() == datetime.date( datetime.strptime( timestamp, "%Y-%m-%d %H:%M:%S")):
-        if device_watt_hour <= value:
-            print( timestamp, " Power on device: ", value)
-
+        ## Cycle through all devices to check their power
+        for devwatt in devices_watt_hour:
+            if devwatt[1] <= value:
+                print( timestamp, " Start your ", devwatt[0], " as the solar power is expected to be ", value, "Wh.")
 
 
 
